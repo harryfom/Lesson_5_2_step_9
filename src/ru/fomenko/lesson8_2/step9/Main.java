5aba1cc8657f6516b0782b8dd5297ab982b89537
FomeIV <harryfom@gmail.com>
2016-03-24 11:14:06 +0300

    5.2 Потоки байт – Шаг 9 end

--- src/ru/fomenko/lesson8_2/step9/Main.java
+++ src/ru/fomenko/lesson8_2/step9/Main.java
@@ -26,8 +26,37 @@ package ru.fomenko.lesson8_2.step9;
  В System.out должны быть выведены байты: 65 10 10 13
  */
 
+import java.io.*;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+//        test();
+        convert(System.in,System.out);
+    }
+
+    private static void test() throws IOException {
         System.out.println("5.2 Потоки байт – Шаг 9");
+        byte[] test = new byte[]{65, 13, 10, 10, 13};
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream(10);
+
+        convert(new ByteArrayInputStream(test),outputStream );
+        byte[] out = outputStream.toByteArray();
+        for (int i: out) {
+            System.out.println(i);
+        }
+    }
+
+    private static void convert(InputStream inputStream,OutputStream outputStream) throws IOException {
+        int buf1 = inputStream.read();
+        int buf2;
+
+        while(buf1 != -1){
+            buf2 = inputStream.read();
+            if (!(buf2 == 10 && buf1 == 13)){
+                outputStream.write(buf1);
+            }
+            buf1 = buf2;
+        }
+        outputStream.flush();
     }
 }
